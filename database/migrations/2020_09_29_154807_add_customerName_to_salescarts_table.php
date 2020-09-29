215ce357a18e6cbc7a3db7e31995f1d25c59bb1f
example1 <32572574+bestshine@users.noreply.github.com>
2020-09-29 15:37:19 +0530

    initial commit
    initial commit

---
+++ database/migrations/2020_09_29_154807_add_customerName_to_salescarts_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddCustomerNameToSalescartsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('salescarts', function (Blueprint $table) {
+            $table->string('customerName')->nullable();
+            $table->string('phone_no')->nullable();
+            $table->string('address')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('salescarts', function (Blueprint $table) {
+            //
+        });
+    }
+}
